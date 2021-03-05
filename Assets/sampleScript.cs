c0769ab475b1d22323d898eda680def80b027b4b
maimoon04 <moonishtiaq5@gmail.com>
2021-03-05 11:42:47 +0500

    initial commit

---
+++ Assets/sampleScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class sampleScript : MonoBehaviour
+{
+    public Text text;
+    public Slider slider;
+   public Image image;
+    public GameObject block;
+    public int width = 4;
+    public int height = 4;
+    public int breadth = 4;
+
+    void Start()
+    {
+        for (int z = 0; z < breadth; ++z)
+        {
+          for (int y = 0; y < height; ++y)
+          {
+            for (int x = 0; x < width; ++x)
+            {
+              Instantiate(block, new Vector3(x, y, z), Quaternion.identity);
+            }
+           }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     int o=System.DateTime.Now.Second;
+        Debug.Log(o);
+    }
+
+    public void imageColor()
+    {
+        image.color = new Color32(255, 0, 0, 255);
+    }
+
+    public void sliderValue()
+    {
+        text.text = slider.value.ToString();
+    }
+
+}
+
